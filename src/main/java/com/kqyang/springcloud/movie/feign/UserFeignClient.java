6139d00b65a4d5651de1173b576590bbd5e15192
kqyang <kqyang0000@163.com>
2019-08-24 15:50:39 +0800

    feignClient used actuator of hystrix

--- src/main/java/com/kqyang/springcloud/movie/feign/UserFeignClient.java
+++ src/main/java/com/kqyang/springcloud/movie/feign/UserFeignClient.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author kqyang on 2019/8/23
