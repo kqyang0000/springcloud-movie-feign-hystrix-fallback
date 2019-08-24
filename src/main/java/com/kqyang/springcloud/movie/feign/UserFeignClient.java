8fcaa6bf30bb98853bfc15b84ed303d126155afd
kqyang <kqyang0000@163.com>
2019-08-24 11:57:40 +0800

    add request fallback status

--- src/main/java/com/kqyang/springcloud/movie/feign/UserFeignClient.java
+++ src/main/java/com/kqyang/springcloud/movie/feign/UserFeignClient.java
@@ -1,6 +1,9 @@
 package com.kqyang.springcloud.movie.feign;
 
 import com.kqyang.springcloud.movie.entity.User;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * @author kqyang on 2019/8/23
  */
-@FeignClient(name = "springcloud-provider-user", fallback = UserFeignClient.FeignClientFallback.class)
+@FeignClient(name = "springcloud-provider-user", fallback = UserFeignClient.FeignClientFallback.class,
+        fallbackFactory = UserFeignClient.FeignClientFallbackFactory.class)
 public interface UserFeignClient {
 
     /**
@@ -34,6 +38,25 @@ public interface UserFeignClient {
             user.setId(-1L);
             user.setName("默认用户");
             return user;
+        }
+    }
+
+    /**
+     * 回退状态打印
+     */
+    @Component
+    class FeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
+        private static final Logger LOGGER = LoggerFactory.getLogger(FeignClientFallbackFactory.class);
+
+        @Override
+        public UserFeignClient create(final Throwable throwable) {
+            return (id -> {
+                LOGGER.error("fallback reason: " + throwable);
+                User user = new User();
+                user.setId(-1L);
+                user.setName("默认用户");
+                return user;
+            });
         }
     }
 }
